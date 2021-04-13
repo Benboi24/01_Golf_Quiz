38714c48faf1bb6dc88124ee60a997604ddcdec7
Benboi24 <60630518+Benboi24@users.noreply.github.com>
2021-04-14 09:16:02 +1200

    start_GUI_v1.py
    beginning

---
+++ Start_GUI_v1.py
@@ -0,0 +1,51 @@
+from tkinter import *
+from functools import partial  # To prevent unwanted windows
+import random
+
+
+class Start:
+    def __init__(self, parent):
+
+        # GUI to get starting balance and stakes
+        self.start_frame = Frame(padx=10, pady=10)
+        self.start_frame.grid()
+
+        # Mystery Heading (row 0)
+        self.mystery_box_label = Label(self.start_frame, text="Golf Quiz",
+                                       font="Arial 19 bold")
+        self.mystery_box_label.grid(row=0)
+
+        # Initial Instructions (row 1)
+        self.mystery_instructions = Label(self.start_frame, font="Arial 10 italic",
+                                          text=" Welcome to the Golf Quiz ",
+                                               "",
+                                          wrap=275, justify=LEFT, padx=10, pady=10)
+        self.mystery_instructions.grid(row=1)
+
+        # Entry box... (row 2)
+        self.start_amount_entry = Entry(self.start_frame, font="Arial 16 bold")
+        self.start_amount_entry.grid(row=2)
+
+        # button frame (row 3)
+        self.stakes_frame = Frame(self.start_frame)
+        self.stakes_frame.grid(row=3)
+
+        # Buttons go here...
+        button_font = "Arial 12 bold"
+        # Orange low stakes button...
+        self.low_stakes_button = Button(self.stakes_frame, text="Start",
+                                        command=lambda: self.to_game(1),
+                                        font=button_font, bg="#FF9933")
+        self.low_stakes_button.grid(row=0, column=0, pady=10)
+
+    class Game:
+        def __init__(self, partner, stakes, starting_balance):
+            print(stakes)
+            print(starting_balance)
+
+# main routine
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Mystery Box Game")
+    something = Start(root)
+    root.mainloop()
