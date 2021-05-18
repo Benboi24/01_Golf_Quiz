30fa6937f198d472c81d23ed891d722529fb7224
Benboi24 <60630518+Benboi24@users.noreply.github.com>
2021-05-19 09:41:42 +1200

    Start_GUI_v2.py
    Not the exact layout i planned but has all the buttons labels, titles etc. Good for now will move on

--- Start_GUI_v2.py
+++ Start_GUI_v2.py
@@ -19,7 +19,7 @@ class Start:
                                        font="Arial 19 bold")
         self.start_frame_label.grid(row=0)
 
-        # Initial Instructions (row 1)
+        # Initial Instructions, Question instructions (row 1 & 2)
         self.quiz_instructions = Label(self.start_frame, font="Arial 10 italic",
                                           text=" Welcome to the Golf Quiz Game. "
                                                " Each question will be randomly "
@@ -29,33 +29,39 @@ class Start:
                                           wrap=275, justify=LEFT, padx=10, pady=10)
         self.quiz_instructions.grid(row=1)
 
+        self.question_instructions = Label(self.start_frame, font="Arial 10 bold",
+                                                text="Below input how many questions " 
+                                                " you want to play with:",
+                                                wrap=275, justify=LEFT, padx=10, pady=10)
+        self.question_instructions.grid(row=2)
+
         # Question Heading, entry box & Button (row 2)
         self.entry_error_frame = Frame(self.start_frame, width=10)
-        self.entry_error_frame.grid(row=2)
+        self.entry_error_frame.grid(row=3)
 
         self.start_amount_entry = Entry(self.entry_error_frame, 
                                         font="Arial 19 bold", width=10)
-        self.start_amount_entry.grid(row=3)
+        self.start_amount_entry.grid(row=4)
 
         self.number_error_label = Label(self.start_frame, fg="blue",
                                         text="", font="Arial 10 bold",
                                         justify=LEFT)
-        self.number_error_label.grid(row=3)
+        self.number_error_label.grid(row=4)
 
         # button frame (row 3)
         self.start_frame = Frame(self.start_frame)
-        self.start_frame.grid(row=4)
+        self.start_frame.grid(row=5)
 
         # Buttons go here...
         button_font = "Arial 12 bold"
         # Orange start button
         self.start_button = Button(self.start_frame, text="Start",
                                    font=button_font, bg="#FF9933")
-        self.start_button.grid(row=4, column=0, pady=10)
+        self.start_button.grid(row=5, column=0, pady=5)
         # Blue select button
         self.select_button = Button(self.start_frame, text="Select",
                                     font=button_font, bg="#3399FF")
-        self.select_button.grid(row=3, column=0, pady=10)
+        self.select_button.grid(row=4, column=0, pady=5)
 
     def number_amount(self): 
         number_balance = self.number_amount_entry.get()
