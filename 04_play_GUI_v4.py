f86cb871d8e9fe373e2d3eef3a8010bbb0a1a27c
Benboi24 <60630518+Benboi24@users.noreply.github.com>
2021-06-08 10:39:13 +1200

    04_play_GUI_v4.py
    The layout is alot better. Only need the next button to be wider across the buttons then the layout should be good.

--- 04_play_GUI_v4.py
+++ 04_play_GUI_v4.py
@@ -6,6 +6,9 @@ import random
 class Start:
     def __init__(self, parent):
 
+        # Buttons styling here...
+        button_font = "Arial 12 bold"
+
         # GUI to get user to start game
         self.play_frame = Frame(padx=10, pady=10)
         self.play_frame.grid()
@@ -29,38 +32,49 @@ class Start:
         self.entry_error_frame = Frame(self.play_frame, width=10)
         self.entry_error_frame.grid(row=3)
 
-        self.answer_entry = Entry(self.play_frame, font="Arial 19 bold", width=10)
-        self.answer_entry.grid(row=3)
+        self.answer_entry = Entry(self.entry_error_frame, font="Arial 19 bold", width=10)
+        self.answer_entry.grid(row=0)
+
+        # Yellow Select Button
+        self.select_button = Button(self.entry_error_frame, text="Select",
+                                   font=button_font, bg="#FFFF33")
+        self.select_button.grid(row=0, column=1, pady=10, padx=5)
 
         # Will have total questions each time you answer one
         self.number_error_label = Label(self.play_frame, fg="red",
-                                        text="", font="Arial 10 bold",
+                                        text="errors go here", font="Arial 10 bold",
                                         justify=LEFT)
-        self.number_error_label.grid(row=4)
+        self.number_error_label.grid(row=2)
+
+        # Next Button (row 4)
+        self.next_frame = Frame(self.play_frame, width=100)
+        self.next_frame.grid(row=4)
+
+        self.next_button = Button(self.next_frame, text="Next",
+                                 font=button_font, bg="#FF0080")
+        self.next_button.grid(row=0, column=1, padx=5, pady=10)
+
+        # Help, Status & Dismiss Buttons 
+        self.hsd_frame = Frame(self.play_frame)
+        self.hsd_frame.grid(row=5)
+
+        # Green Help Button
+        self.help_button = Button(self.hsd_frame, text="Help",
+                                   font=button_font, bg="#009900")
+        self.help_button.grid(row=0, column=0, padx=5)
+
+        # Blue Stats Button
+        self.stats_button = Button(self.hsd_frame, text="Stats",
+                                   font=button_font, bg="#FF0000")
+        self.stats_button.grid(row=0, column=1, padx=5)
+        # Purple Dismiss Button
+        self.dismiss_button = Button(self.hsd_frame, text="Dismiss",
+                                    font=button_font, bg="#B266FF")
+        self.dismiss_button.grid(row=0, column=2, padx=5)
 
         # button frame (row 3)
         self.start_frame = Frame(self.play_frame)
-        self.start_frame.grid(row=5)
-
-        # Buttons go here...
-        button_font = "Arial 12 bold"
-        
-        # Yellow Next Button
-        self.next_button = Button(self.play_frame, text="Next",
-                                   font=button_font, bg="#FFFF33")
-        self.next_button.grid(row=3, column=1, pady=15)
-        # Green Help Button
-        self.help_button = Button(self.play_frame, text="Help",
-                                   font=button_font, bg="#009900")
-        self.help_button.grid(row=5, column=0, pady=10)
-        # Blue Stats Button
-        self.stats_button = Button(self.play_frame, text="Stats",
-                                   font=button_font, bg="#FF0000")
-        self.stats_button.grid(row=5, column=1, pady=10, padx=5)
-        # Purple Dismiss Button
-        self.dismiss_button = Button(self.play_frame, text="Dismiss",
-                                    font=button_font, bg="#B266FF")
-        self.dismiss_button.grid(row=5, column=2, pady=10, padx=15)
+        self.start_frame.grid(row=5)     
 
     def to_play(self):
 
